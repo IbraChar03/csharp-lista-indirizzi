a195098a3b67b2fde59bbeb0bc225f28024521ae
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-14 15:01:22 +0200

    Add project files.

---
+++ csharp-lista-indirizzi/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace csharp_lista_indirizzi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StreamReader testo = File.OpenText("c:\\addresses.csv");
+                while(!testo.EndOfStream)
+                {
+                    string riga = testo.ReadLine();
+                    Console.WriteLine(riga);
+                }
+
+            }
+            catch { 
+            }
+
+        }
+    }
+}
