99999b07d359aee611662451cd41a4de1028e460
Ibrahim Char <113177312+IbraChar03@users.noreply.github.com>
2023-04-14 15:29:07 +0200

    stampa indirizzi

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -4,6 +4,7 @@
     {
         static void Main(string[] args)
         {
+            List<Indirizzo> indirizzi = new List<Indirizzo>();
 
 
             try
@@ -13,12 +14,27 @@
                 {
                     string riga = testo.ReadLine();
                     var splitTesto = riga.Split(",");
+                 
+                    Indirizzo indirizzo = new Indirizzo((splitTesto[0] == "") ? "Non definito" : splitTesto[0], (splitTesto[1] == "") ? "Non definito" : splitTesto[1], (splitTesto[2] == "") ? "Non definito" : splitTesto[2], (splitTesto[3] == "") ? "Non definito" : splitTesto[3], (splitTesto[4] == "") ? "Non definito" : splitTesto[4], (splitTesto[5] == "") ? "Non definito" : splitTesto[5]);
+                    indirizzi.Add(indirizzo);
+
 
 
                 }
 
             }
-            catch { 
+            catch (Exception e) {
+                Console.WriteLine(e.Message);
+            }
+            foreach(Indirizzo ind in indirizzi)
+            {
+                Console.WriteLine("INDIRIZZO :");
+                Console.WriteLine($"Nome : {ind.Nome}");
+                Console.WriteLine($"Cognome : {ind.Cognome}");
+                Console.WriteLine($"Via : {ind.Via}");
+                Console.WriteLine($"Città : {ind.Città}");
+                Console.WriteLine($"Provincia : {ind.Provincia}");
+                Console.WriteLine($"Cap : {ind.Cap}");
             }
 
         }
